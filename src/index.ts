74ff19058383572ef5c96a0484fd2256141ed719
Dennis <codetitan2206@gmail.com>
2025-03-29 01:27:52 -0700

    chore: set up .env configuration and provide env.example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from "express";
+import { PORT } from "./secret";
 
-const app:Express = express();
-app.get("/", (req:Request, res:Response)=>{
-    res.send("working")
-})
+const app: Express = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("working");
+});
 
-app.listen(3000, ()=>{console.log("App Working!")})
+app.listen(PORT, () => {
+  console.log("App Working!");
+});
