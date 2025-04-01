a4960dbb84e1198eff3e2f361e7ef6487786257c
Dennis <codetitan2206@gmail.com>
2025-04-01 00:24:05 -0700

    feat: implement user login with JWT authentication

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { prismaClient } from '..';
-import { hashSync } from 'bcrypt';
+import { hashSync, compareSync } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secret';
 
 export const signup = async (req: Request, res: Response) => {
   const { email, name, password } = req.body;
@@ -16,4 +18,22 @@ export const signup = async (req: Request, res: Response) => {
     },
   });
   res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw Error('User does not exist');
+  }
+  if (!compareSync(password, user.passwod)) {
+    throw Error('Incorrect Password');
+  }
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
+  res.json({ user, token });
 };
