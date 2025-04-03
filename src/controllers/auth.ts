e4e84407880bb352bffe26e31ff0ff4899db0db4
Dennis <codetitan2206@gmail.com>
2025-04-03 05:03:14 -0700

    feat: implement advanced error handling in Node.js with Prisma

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,14 +1,16 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { prismaClient } from '..';
 import { hashSync, compareSync } from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../secret';
+import { BadRequestsExceptions } from '../exceptions/bad-requests';
+import { ErrorCodes } from '../exceptions/root';
 
-export const signup = async (req: Request, res: Response) => {
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
   const { email, name, password } = req.body;
   let user = await prismaClient.user.findFirst({ where: { email } });
   if (user) {
-    throw Error('User already exist');
+    next(new BadRequestsExceptions('User already Exists', ErrorCodes.USER_ALREADY_EXISTS));
   }
   user = await prismaClient.user.create({
     data: {
