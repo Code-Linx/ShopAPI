01bd58f358ef887d48a2897df53e3ccde988421a
Dennis <codetitan2206@gmail.com>
2025-04-09 07:32:48 -0700

    feat: implement authentication middleware with JWT decoding using Prisma ORM

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -43,3 +43,7 @@ export const login = async (req: Request, res: Response) => {
   );
   res.json({ user, token });
 };
+
+export const me = async (req: Request, res: Response) => {
+  res.json(req.user);
+};
