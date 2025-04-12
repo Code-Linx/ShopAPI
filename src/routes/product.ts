85877e3629d26fe2e5df5257253b0a64d300d1f7
Dennis <codetitan2206@gmail.com>
2025-04-12 04:28:05 -0700

    feat: implement product update, delete, list, and getById controllers & update product description type to Text in Prisma schema

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import { errorHandler } from '../error-handler';
-import { createProduct } from '../controllers/product';
+import {
+  createProduct,
+  deleteProduct,
+  getProductByID,
+  listProduct,
+  updateProduct,
+} from '../controllers/product';
 import authMiddleWare from '../middlewares/auth';
 import adminMiddleWare from '../middlewares/admin';
 
 const productRoutes: Router = Router();
 productRoutes.post('/', [authMiddleWare, adminMiddleWare], errorHandler(createProduct));
+productRoutes.put('/:id', [authMiddleWare, adminMiddleWare], errorHandler(updateProduct));
+productRoutes.delete('/:id', [authMiddleWare, adminMiddleWare], errorHandler(deleteProduct));
+productRoutes.get('/', [authMiddleWare, adminMiddleWare], errorHandler(listProduct));
+productRoutes.get('/:id', [authMiddleWare, adminMiddleWare], errorHandler(getProductByID));
 export default productRoutes;
