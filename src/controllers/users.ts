ac7a756286b9a411a495f7956a60fc1921a0e866
Dennis <codetitan2206@gmail.com>
2025-04-17 15:15:02 -0700

    feat: add delete and list address controllers

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { AddressSchema } from '../schema/users';
 import { prismaClient } from '..';
+import { NotFoundExceptions } from '../exceptions/not-found';
+import { ErrorCodes } from '../exceptions/root';
 
 export const addAddress = async (req: Request, res: Response) => {
   AddressSchema.parse(req.body);
@@ -13,5 +15,24 @@ export const addAddress = async (req: Request, res: Response) => {
   });
   res.json(address);
 };
-export const deleteAddress = async (req: Request, res: Response) => {};
-export const listAddress = async (req: Request, res: Response) => {};
+export const deleteAddress = async (req: Request, res: Response) => {
+  try {
+    await prismaClient.address.delete({
+      where: {
+        id: +req.params.id,
+      },
+    });
+    res.json({ success: true });
+  } catch (err) {
+    throw new NotFoundExceptions('Address Not Found.', ErrorCodes.ADDRESS_NOT_FOUND);
+  }
+};
+
+export const listAddress = async (req: Request, res: Response) => {
+  const address = await prismaClient.address.findMany({
+    where: {
+      userId: req.user.id,
+    },
+  });
+  res.json({ address });
+};
