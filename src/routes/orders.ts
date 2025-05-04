c4bf0f6a1c5cf11f1bc80bb5e74af71710d79aae
Dennis <codetitan2206@gmail.com>
2025-05-04 01:01:39 -0700

    feat(admin): add order management and user-specific order endpoints

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import authMiddleWare from '../middlewares/auth';
 import { errorHandler } from '../error-handler';
-import { cancelOrder, createOrder, geteOrderById, listOrders } from '../controllers/orders';
+import {
+  cancelOrder,
+  changeStatus,
+  createOrder,
+  geteOrderById,
+  listAllOrders,
+  listOrders,
+  listUsersOrders,
+} from '../controllers/orders';
+import adminMiddleWare from '../middlewares/admin';
 
 const orderRoutes: Router = Router();
 
 orderRoutes.post('/', [authMiddleWare], errorHandler(createOrder));
 orderRoutes.get('/', [authMiddleWare], errorHandler(listOrders));
 orderRoutes.put('/:id/cancel', [authMiddleWare], errorHandler(cancelOrder));
+orderRoutes.get('/index', [authMiddleWare], [adminMiddleWare], errorHandler(listAllOrders));
+orderRoutes.get('/:id/user', [authMiddleWare], [adminMiddleWare], errorHandler(listUsersOrders));
+orderRoutes.put('/:id/status', [authMiddleWare], [adminMiddleWare], errorHandler(changeStatus));
 orderRoutes.get('/:id', [authMiddleWare], errorHandler(geteOrderById));
 export default orderRoutes;
